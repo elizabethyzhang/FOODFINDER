994412822cd0012d32d62d27eda8280c0252f0bf
Elizabeth Zhang <lizyzhang@gmail.com>
2020-03-08 16:39:12 -0400

    Updates

---
+++ precache-manifest.fc7cb0fd7029ef287f56f1b0b75dee59.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e770990033facbed76511f98026eed9",
+    "url": "/FOODFINDER/index.html"
+  },
+  {
+    "revision": "dfd47dca00175285c12c",
+    "url": "/FOODFINDER/static/css/main.2e31bd03.chunk.css"
+  },
+  {
+    "revision": "786060155e55f0f2134a",
+    "url": "/FOODFINDER/static/js/2.e654fe76.chunk.js"
+  },
+  {
+    "revision": "541f9b41429990ef927f5ab96a7f2763",
+    "url": "/FOODFINDER/static/js/2.e654fe76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfd47dca00175285c12c",
+    "url": "/FOODFINDER/static/js/main.77dad6d2.chunk.js"
+  },
+  {
+    "revision": "daff2d6ca1a90712d4c8",
+    "url": "/FOODFINDER/static/js/runtime-main.a5c917f9.js"
+  },
+  {
+    "revision": "ca35a992a488ed7c1dafae891c631937",
+    "url": "/FOODFINDER/static/media/horizontal_bg.ca35a992.jpg"
+  },
+  {
+    "revision": "cb3fdcdde9fc77ec6647c05f9714e100",
+    "url": "/FOODFINDER/static/media/vertical_bg.cb3fdcdd.jpg"
+  }
+]);
